3d407ca070e20261804445f729871fa8dc1d3a4b
Aleksandr Dus <aleksandr.dus@scompler.com>
2025-02-11 22:52:34 +0300

    chore: reset ignored files

--- test/unit/Raffle.test.ts
+++ test/unit/Raffle.test.ts
@@ -2,7 +2,7 @@ import { ethers, getNamedAccounts, deployments } from 'hardhat'
 import type { Raffle } from '../../typechain-types'
 import { expect } from 'chai'
 
-// const getAmount = (value: number) => ethers.parseEther(value.toString())
+const getAmount = (value: number) => ethers.parseEther(value.toString())
 
 describe('Raffle', async () => {
   let raffle: Raffle
@@ -21,6 +21,16 @@ describe('Raffle', async () => {
       const tx = raffle.enter()
 
       await expect(tx).to.be.revertedWithCustomError(raffle, 'Raffle__NotEnoughValueEntered')
+    })
+
+    it('adds account to players array', async () => {
+      const txResponse = await raffle.enter({ value: getAmount(1) })
+
+      await txResponse.wait(1)
+
+      const player = await raffle.getPlayer(0)
+
+      expect(player).to.equal(deployer)
     })
   })
 })
